ccc5f6b87cd0a11764d14b1cabcdad0c72f38de0
Yonatan Katz <yonatan.katz@imperva.com>
2018-12-19 09:28:56 +0200

    hello world project

---
+++ logger-test-app/src/main/java/com/jonateam/testApp/TestApp.java
@@ -0,0 +1,10 @@
+package com.jonateam.testApp;
+
+/**
+ * Created by yonatan.katz on 12/19/2018.
+ */
+public class TestApp {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
